26abef0a8289dffcc4d80f4f5472bd5dcae1f597
AKASH KUMAR VERMA <akverma2496@gmail.com>
2025-11-06 14:02:19 +0530

    spacebar-issue-fixed

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: false, // Disable minification
+  },
+  rollupOptions: {
+    treeshake: false,  // Disable tree-shaking
+  },
   base: '/'
   // server: {
   //   historyApiFallback: true, // ✅ ensures SPA routing fallback
